374d5487ee7fc27b2b7fd9946d5d0bd643a04014
Bearbrice <berclazpro@gmail.com>
2020-05-24 10:29:06 +0200

    Major bug solved Bug with localhost not found because the wcf service was kind of sleeping

--- WindowsFormsTestWebService/Program.cs
+++ WindowsFormsTestWebService/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsTestWebservice.ServiceReferencePayment;
 
 namespace WindowsFormsTestWebservice
 {
@@ -14,9 +16,11 @@ namespace WindowsFormsTestWebservice
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
